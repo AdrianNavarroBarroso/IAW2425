b412b1f75a4800c80b237cab023533a467a91aea
Adrián Navarro Barroso <adriannavarro05@iesamachado.org>
2024-11-28 14:13:31 +0100

    php

--- adrianPHP/info.php
+++ adrianPHP/info.php
@@ -3,11 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>phpinfo</title>
 </head>
 <body>
-    <?php
-    phpinfo();
-    ?>
+    <?php phpinfo();?>
 </body>
 </html>
---
+++ adrianPHP/mix.php
@@ -0,0 +1,11 @@
+<!DOCKTYPE HTML>
+<html>
+<head>
+	<title>Una web con PHP</title>
+</head>
+<body>
+	<h1>Encabezado</h1>
+	<?php echo "<p>chimpun</p>"; ?>
+</body>
+</html>
+
